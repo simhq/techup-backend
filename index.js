bafb78e09ae01dd1072703eedadcc1c29aca08b3
simhq <hanqiangsim@gmail.com>
2024-11-11 03:49:52 +0000

    bug fix

--- index.js
+++ index.js
@@ -6,8 +6,10 @@ const __dirname = dirname(fileURLToPath(import.meta.url));
 const app = express();
 const port = 3000;
 
+app.use(express.static('public')); 
+
 app.get("/", (req, res) => {
-  res.sendFile(__dirname + "/index.html");
+  res.render("index.ejs");
 });
 
 app.listen(port, () => {
